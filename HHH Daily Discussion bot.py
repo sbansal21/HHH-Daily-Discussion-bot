b519adb3cef2d2a28501b62042e31e2648e517d9
Sumeet Bansal <sumeetbansal@gmail.com>
2017-07-11 22:58:01 -0700

    updated bot to read subreddit submissions

--- HHH Daily Discussion bot.py
+++ HHH Daily Discussion bot.py
@@ -10,3 +10,16 @@ bot = praw.Reddit(user_agent = 'HHH PyBot v1',
                   client_secret = bot.client_secret,
                   username = bot.username,
                   password = bot.password)
+
+# points bot to a specific subreddit
+subreddit = bot.subreddit('hiphopheads')
+
+# prints given number of posts from subreddit
+posts = 10
+for submission in subreddit.hot(limit = posts):
+    print(submission.title)
+    print("\tat ", submission.score, "\tby ", submission.author)
+    print(submission.selftext)
+    if len(submission.selftext) > 0:
+        print()
+    print("---------------------------------\n")
