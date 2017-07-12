cc8b63d2834205775f052a03fc82cdc1551666fa
Sumeet Bansal <sumeetbansal@gmail.com>
2017-07-11 23:42:18 -0700

    updated bot to search for and reply to specific threads

--- HHH Daily Discussion bot.py
+++ HHH Daily Discussion bot.py
@@ -3,9 +3,19 @@
 import bot  # contains private bot info
 
 import praw
+import re
+import os
 
-# creates praw Reddit instance
-bot = praw.Reddit(user_agent = 'HHH PyBot v1',
+# tracks which posts have already been visited
+if not os.path.isfile("visited.txt"):
+    visited = []
+else:
+    with open("visited.txt", "r") as f:
+        visited = f.read().split("\n")
+        visited = list(filter(None, visited))
+
+# creates PRAW Reddit instance
+bot = praw.Reddit(user_agent = 'HHH PyBot',
                   client_id = bot.client_id,
                   client_secret = bot.client_secret,
                   username = bot.username,
@@ -14,12 +24,21 @@ bot = praw.Reddit(user_agent = 'HHH PyBot v1',
 # points bot to a specific subreddit
 subreddit = bot.subreddit('hiphopheads')
 
-# prints given number of posts from subreddit
-posts = 10
+# iterates through 100 newest posts in subreddit
+posts = 100
+post = "Daily Discussion Thread"
+comment = "yeezy yeezy what's good"
+found = False
 for submission in subreddit.hot(limit = posts):
-    print(submission.title)
-    print("\tat ", submission.score, "\tby ", submission.author)
-    print(submission.selftext)
-    if len(submission.selftext) > 0:
-        print()
-    print("---------------------------------\n")
+    if submission.id not in visited:
+        if re.search(post, submission.title, re.IGNORECASE):
+            submission.reply(comment)
+            found = True
+            print("bot successfully commented in thread ", submission.id)
+            visited.append(submission.id)
+if not found:
+    print("specified thread not found.")
+
+with open("visited.txt", "w") as f:
+    for post in visited:
+        f.write(post + "\n")
