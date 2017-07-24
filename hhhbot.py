2b7b61957b00eba78bcf65de858f597f1fe1e3ee
Sumeet Bansal <sumeetbansal@gmail.com>
2017-07-24 20:24:51 +0000

    made hhhbot more standalone to avoid interference with rbot

--- hhhbot.py
+++ hhhbot.py
@@ -1,12 +1,18 @@
 #!/usr/bin/python3
 
-import bot  # contains private bot info
-
 import datetime
 
 import praw
 import re
 import os
+
+import runpy
+import edit
+
+# ensures settings properly initialized
+runpy.run_path("edit.py")
+
+import settings
 
 # tracks which posts have already been visited
 if not os.path.isfile("visited.txt"):
@@ -18,10 +24,10 @@ else:
 
 # creates PRAW Reddit instance
 bot = praw.Reddit(user_agent = 'HHH PyBot',
-                  client_id = bot.client_id,
-                  client_secret = bot.client_secret,
-                  username = bot.username,
-                  password = bot.password)
+                  client_id = settings.client_id,
+                  client_secret = settings.client_secret,
+                  username = settings.username,
+                  password = settings.password)
 
 # points bot to a specific subreddit
 subreddit = bot.subreddit('hiphopheads')
@@ -31,21 +37,18 @@ success = "bot successfully commented in thread "
 failure = "specified thread not found."
 
 # iterates through 100 newest posts in subreddit
-posts = 100
-post = "Daily Discussion Thread"
+trigger = "Daily Discussion Thread"
 comment = "yeezy yeezy what's good"
-
 found = False
-for submission in subreddit.new(limit = posts):
-    if submission.id not in visited:
-        if re.search(post, submission.title, re.IGNORECASE):
-            submission.reply(comment)
-            found = True
-            print(timestamp + success + submission.id)
-            visited.append(submission.id)
-if not found:
-    print(timestamp + failure)
+for submission in subreddit.new(limit = 100):
+	if submission.id not in visited:
+		if re.search(trigger, submission.title, re.IGNORECASE):
+			submission.reply(comment)
+			found = True
+			print(timestamp + failure)
+			visited.append(submission.id)
 
+# logs triggered submissions as visited
 with open("visited.txt", "w") as file:
-    for post in visited:
-        file.write(post + "\n")
+	for post in visited:
+		file.write(post + "\n")
